9444be32cc3641685b3bbf9e3ba6e0949cbdf03b
Aaron Charcoal Styles <aaron@futurefriendly.team>
2022-04-01 11:50:23 +1100

    updated header, imported more libs from the design system

--- components/DocumentTitle.tsx
+++ components/DocumentTitle.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 export const DocumentTitle = ({ title }: { title?: string }) => (
   <Head>
     <title>
-      {[title, "Agriculture Design System"].filter(Boolean).join(" | ")}
+      {[title, "Export Service"].filter(Boolean).join(" | ")}
     </title>
   </Head>
 );
--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,11 +1,23 @@
 import { useRouter } from "next/router";
 import { Logo } from "@ag.ds-next/ag-branding";
-import { Stack } from "@ag.ds-next/box";
+import { Box, Flex, Stack } from "@ag.ds-next/box";
 import { Header } from "@ag.ds-next/header";
-import { ExternalLinkIcon } from "@ag.ds-next/icon";
-import { MainNav, MainNavLink } from "@ag.ds-next/main-nav";
+import { MainNav, NavListItem, NavListLink } from "@ag.ds-next/main-nav";
+import { Button } from "@ag.ds-next/button";
+import Link from "next/link";
+import {
+  SearchBox,
+  SearchBoxInput,
+  SearchBoxButton,
+} from "@ag.ds-next/search-box";
 
-const NAV_LINKS = [{ label: "Home", href: "/" }];
+const NAV_LINKS: Array<NavListLink> = [
+  { label: "Home", href: "/" },
+  { label: "New exporters" },
+  { label: "Export operations" },
+  { label: "Updates and insights" },
+  { label: "Contact us" },
+];
 
 export const SiteHeader = () => {
   const router = useRouter();
@@ -14,9 +26,19 @@ export const SiteHeader = () => {
       <Header
         variant="dark"
         logo={<Logo />}
-        heading="Agriculture Design System"
-        subline="Design System for the Export Service"
+        heading="Export Service"
+        subline="Supporting Australian agricultural exports"
         badgeLabel="Alpha"
+        rightContent={
+          <SearchBox onSubmit={console.log}>
+            <SearchBoxInput label="Search this website" />
+            <SearchBoxButton
+              iconOnly={{ xs: false, md: true, lg: false, xl: false }}
+            >
+              Search
+            </SearchBoxButton>
+          </SearchBox>
+        }
       />
       <MainNav
         id="main-nav"
@@ -24,13 +46,18 @@ export const SiteHeader = () => {
         links={NAV_LINKS}
         activePath={router.asPath}
         rightContent={
-          <MainNavLink
-            label="Github"
-            href="https://github.com/steelthreads/agds-next-starter-kit"
-            icon={ExternalLinkIcon}
-            target="_blank"
-            rel="nofollow noreferrer"
-          />
+          <Flex gap={1} justifyContent="space-between" alignItems="flex-start">
+            <Box>
+              <NavListItem>
+                <Link href="">Sign in</Link>
+              </NavListItem>
+            </Box>
+            <Box paddingTop={0.25}>
+              <Button variant="secondary" size="md">
+                Create an account
+              </Button>
+            </Box>
+          </Flex>
         }
       />
     </Stack>
