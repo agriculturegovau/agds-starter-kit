8285f2c4d28a70c0b9975282926d29a8746ac468
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-15 12:39:41 +1000

    Formatting

--- components/DocumentTitle.tsx
+++ components/DocumentTitle.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export const DocumentTitle = ({ title }: { title?: string }) => (
-  <Head>
-    <title>
-      {[title, "Agriculture Design System"].filter(Boolean).join(" | ")}
-    </title>
-  </Head>
+	<Head>
+		<title>
+			{[title, 'Agriculture Design System'].filter(Boolean).join(' | ')}
+		</title>
+	</Head>
 );
